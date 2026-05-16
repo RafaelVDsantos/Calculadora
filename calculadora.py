a990af73f9e1087b748b1e99457b04e03c13992d
SeuNome <rafael.santos@ectare.com.br>
2026-05-16 13:17:31 -0300

    código duplicado na classe principal

--- calculadora.py
+++ calculadora.py
@@ -16,19 +16,3 @@ class Calculadora:
     
     def potenciacao(self, base, expoente):
         return base ** expoente
-
-class Calculadora:
-    def soma(self, a, b):
-        return a + b
-
-    def subtracao(self, a, b):
-        return a - b
-
-    def multiplicacao(self, a, b):
-        return a * b
-
-    def divisao(self, a, b):
-        if b == 0:
-            raise ValueError("O divisor não pode ser zero.")
-        return a / b 
-        main
